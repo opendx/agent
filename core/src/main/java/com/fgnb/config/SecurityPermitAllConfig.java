091f3be8d70290167231ccde181cbe912f45b64a
jiangyitao <451988022@163.com>
2018-12-15 22:32:10 +0800

    支持监听agent状态变化

---
+++ core/src/main/java/com/fgnb/config/SecurityPermitAllConfig.java
@@ -0,0 +1,18 @@
+package com.fgnb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by jiangyitao.
+ * spring-boot-admin 配置
+ */
+@Configuration
+public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();
+    }
+}
