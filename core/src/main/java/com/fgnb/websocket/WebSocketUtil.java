7506d90e3ff5aa43ad1d9aa8105d3062814e114c
jiangyitao <451988022@163.com>
2019-03-08 23:53:33 +0800

    update

--- core/src/main/java/com/fgnb/websocket/WebSocketUtil.java
+++ core/src/main/java/com/fgnb/websocket/WebSocketUtil.java
@@ -1,6 +1,5 @@
 package com.fgnb.websocket;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 
 import javax.websocket.Session;
@@ -9,24 +8,18 @@ import java.io.IOException;
 /**
  * Created by jiangyitao.
  */
-@Slf4j
 public class WebSocketUtil {
 
-    public static void sendText(Session session,String msg) throws Exception{
-        if(session == null){
+    public static void sendText(Session session, String msg) throws IOException {
+        if (session == null) {
             throw new RuntimeException("session不能为空");
         }
-        if(!session.isOpen()){
+        if (!session.isOpen()) {
             throw new RuntimeException("session没有打开");
         }
-        if(StringUtils.isEmpty(msg)){
+        if (StringUtils.isEmpty(msg)) {
             throw new RuntimeException("信息不能为空");
         }
-        try {
-            session.getBasicRemote().sendText(msg);
-        }catch (IOException e){
-            log.error("发送信息失败",e);
-            throw e;
-        }
+        session.getBasicRemote().sendText(msg);
     }
 }
