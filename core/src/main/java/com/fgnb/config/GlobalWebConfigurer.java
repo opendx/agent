08c64d3b0968530183e8e408c9bacd0512a05212
jiangyitao <451988022@163.com>
2019-03-08 23:57:21 +0800

    update

--- core/src/main/java/com/fgnb/config/GlobalWebConfigurer.java
+++ core/src/main/java/com/fgnb/config/GlobalWebConfigurer.java
@@ -2,13 +2,13 @@ package com.fgnb.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Created by jiangyitao.
  */
 @Configuration
-public class GlobalWebConfigurer extends WebMvcConfigurerAdapter {
+public class GlobalWebConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").//所有请求，解决跨域问题
