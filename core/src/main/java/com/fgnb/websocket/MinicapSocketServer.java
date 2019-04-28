c4e96873e9153f783825a7fce5f834d6706ff9c5
jiangyitao <451988022@163.com>
2019-04-29 01:30:48 +0800

    update

--- core/src/main/java/com/fgnb/websocket/MinicapSocketServer.java
+++ core/src/main/java/com/fgnb/websocket/MinicapSocketServer.java
@@ -19,111 +19,96 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by jiangyitao.
- * minicap
  */
 @Slf4j
-@Component //虽然@Component默认是单例模式的，但springboot还是会为每个websocket连接初始化一个bean
-@ServerEndpoint(value = "/minicap/{deviceId}/{userName}")
+@Component
+@ServerEndpoint(value = "/minicap/{deviceId}/{username}")
 public class MinicapSocketServer {
 
-    public static Map<String, Session> minicapSessionMap = new ConcurrentHashMap<>();
+    private static Map<String, Session> sessionPool = new ConcurrentHashMap<>();
 
-    private String deviceId;
-    private Session session;
     private Minicap minicap;
-
-    private MasterApi masterApi = App.getBean(MasterApi.class);
+    private String deviceId;
 
     @OnOpen
-    public void onOpen(@PathParam("deviceId") String deviceId, @PathParam("userName") String userName, Session session) throws Exception {
-
-        this.session = session;
+    public void onOpen(@PathParam("deviceId") String deviceId, @PathParam("username") String username, Session session) throws Exception {
+        log.info("[{}][minicap][socketserver]onOpen: username -> {}", deviceId, username);
         this.deviceId = deviceId;
 
-        log.info("[{}]已连接MinicapSocketServer，sessionid => {}", deviceId, session.getId());
+        RemoteEndpoint.Basic basicRemote = session.getBasicRemote();
+        basicRemote.sendText("minicap websocket连接成功");
 
-        WebSocketUtil.sendText(session, "minicap websocket连接成功");
-
-        //检测手机是否连接
         AndroidDevice androidDevice = AndroidDeviceHolder.get(deviceId);
         if (androidDevice == null || !androidDevice.isConnected()) {
-            log.info("[{}]设备未连接", deviceId);
-            WebSocketUtil.sendText(session, deviceId + "手机未连接,请稍后重试");
+            basicRemote.sendText(deviceId + "手机未连接");
             return;
         }
-        //检测手机是否闲置
+
         if (androidDevice.getDevice().getStatus() != Device.IDLE_STATUS) {
-            log.info("[{}]设备未处于闲置状态", deviceId);
-            WebSocketUtil.sendText(session, deviceId + "设备未处于闲置状态," + androidDevice.getDevice().getUsername() + "使用中");
+            basicRemote.sendText(deviceId + "设备未处于闲置状态，" + androidDevice.getDevice().getUsername() + "使用中");
             return;
         }
 
-        //如果有其他连接在使用该手机，则不往下处理并提示用户该手机在被占用
-        Session otherSession = minicapSessionMap.get(deviceId);
+        Session otherSession = sessionPool.get(deviceId);
         if (otherSession != null && otherSession.isOpen()) {
-            log.info("[{}]检查到有其他连接正在使用minicap服务,不做任何处理，当前sessionid => {}，正在使用minicap的连接sessionid => {}", deviceId, session.getId(), otherSession.getId());
-            WebSocketUtil.sendText(session, deviceId + "手机正在被" + otherSession.getId() + "连接占用,请稍后重试");
+            basicRemote.sendText(deviceId + "手机正在被" + otherSession.getId() + "连接占用，请稍后重试");
             return;
         }
 
-        //将设备id和session放入map
-        minicapSessionMap.put(deviceId, session);
+        sessionPool.put(deviceId, session);
 
-        WebSocketUtil.sendText(session, "开始启动手机minicap服务");
-
+        basicRemote.sendText("启动minicap服务...");
         minicap = new Minicap(androidDevice);
         minicap.start("408x720", 0);
+        basicRemote.sendText("启动minicap服务完成");
 
-        WebSocketUtil.sendText(session, "minicap图片数据处理中," + "端口为:1" + ",session id:" + session.getId());
-
-        //minicap连接成功 则把手机改为使用中
-        Device device = AndroidDeviceHolder.get(deviceId).getDevice();
+        Device device = androidDevice.getDevice();
         device.setStatus(Device.USING_STATUS);
-        device.setUsername(userName);
-        masterApi.saveDevice(device);
-        log.info("[{}]数据库状态改为使用中", deviceId);
+        device.setUsername(username);
+        App.getBean(MasterApi.class).saveDevice(device);
+        log.info("[{}][minicap][socketserver]数据库状态改为{}使用中", deviceId, username);
 
-        //3.将图片队列里的数据发送给浏览器
         BlockingQueue<byte[]> imgQueue = minicap.getImgQueue();
-        while (minicap.isParseFrame()) {
-            byte[] img = imgQueue.take();
-            session.getBasicRemote().sendBinary(ByteBuffer.wrap(img));
-        }
+        new Thread(() -> {
+            while (minicap.isParseFrame()) {
+                try {
+                    byte[] img = imgQueue.take();
+                    if (session.isOpen()) {
+                        basicRemote.sendBinary(ByteBuffer.wrap(img));
+                    }
+                } catch (Exception e) {
+                    log.error("[{}][minicap][socketserver]图片处理出错", e);
+                }
+            }
+            log.info("[{}][minicap][socketserver]停止发送图片数据", deviceId);
+        }).start();
     }
 
-    /**
-     * socket关闭：minicap资源回收，设备数据库改为闲置
-     * 如果设备已经被其他连接使用，则minicapDataHandler为null 不会释放掉手机的minicap资源
-     */
     @OnClose
     public void onClose() {
-        log.info("[{}]断开MinicapSocketServer连接，sessionid => {}", deviceId, session.getId());
-        //由于可能有多个连接访问进来，只允许一个连接使用，意味着只有minicapDataHandler被成功初始化的连接才是占用的连接，占用连接断开才做资源回收操作
+        log.info("[{}][minicap][socketserver]onClose", deviceId);
         if (minicap != null) {
-            //断开连接后 移除map里的session
-            minicapSessionMap.remove(deviceId);
-            //停止minicap
+            sessionPool.remove(deviceId);
             minicap.stop();
-            //minicap总是比minitouch/adbkit等晚很多关闭，所以只要minicap websocket关闭 则意味着手机可以变为闲置了
+
             Device device = AndroidDeviceHolder.get(deviceId).getDevice();
             //因为手机可能被拔出离线，AndroidDeviceChangeService.deviceDisconnected已经在数据库改为离线，这里不能改为闲置
             if (device != null && device.getStatus() == Device.USING_STATUS) {
-                //将设备改为闲置
                 device.setStatus(Device.IDLE_STATUS);
-                masterApi.saveDevice(device);
-                log.info("[{}]数据库状态改为闲置", deviceId);
+                App.getBean(MasterApi.class).saveDevice(device);
+                log.info("[{}][minicap][socketserver]数据库状态改为闲置", deviceId);
             }
         }
     }
 
     @OnError
     public void onError(Throwable t) {
-        log.error("[{}]minicap websocket onError，sessionid => {}", deviceId, session.getId(), t);
+        log.error("[{}][minicap][socketserver]onError", deviceId, t);
     }
 
 
     @OnMessage
     public void onMessage(String message) {
-        log.info("来自客户端的消息:{} {}", message, session.getId());
+        log.info("[{}][minicap][socketserver]onMessage: {}", deviceId, message);
     }
 }
--- core/src/main/java/com/fgnb/websocket/MinitouchSocketServer.java
+++ core/src/main/java/com/fgnb/websocket/MinitouchSocketServer.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.fgnb.android.AndroidDevice;
 import com.fgnb.android.AndroidDeviceHolder;
+import com.fgnb.android.AndroidUtils;
 import com.fgnb.android.stf.minitouch.Minitouch;
 import com.fgnb.model.Device;
 import lombok.extern.slf4j.Slf4j;
@@ -12,8 +13,6 @@ import org.springframework.stereotype.Component;
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -22,128 +21,115 @@ import java.util.concurrent.ConcurrentHashMap;
  * minitouch
  */
 @Slf4j
-//虽然@Component默认是单例模式的，但springboot还是会为每个websocket连接初始化一个bean
 @Component
 @ServerEndpoint(value = "/minitouch/{deviceId}")
 public class MinitouchSocketServer {
 
-	public static Map<String,Session> minitouchSessionMap = new ConcurrentHashMap<>();
+    public static Map<String, Session> sessionPool = new ConcurrentHashMap<>();
 
-	private String deviceId;
-	private Session session;
+    private Minitouch minitouch;
+    private String deviceId;
+    private AndroidDevice androidDevice;
 
-	private Minitouch minitouch;
+    @OnOpen
+    public void onOpen(@PathParam("deviceId") String deviceId, Session session) throws Exception {
+        log.info("[{}][minitouch][socketserver]onOpen", deviceId);
+        this.deviceId = deviceId;
 
-	@OnOpen
-	public void onOpen(@PathParam("deviceId") String deviceId, Session session) throws Exception {
+        RemoteEndpoint.Basic basicRemote = session.getBasicRemote();
+        basicRemote.sendText("minitouch websocket连接成功");
 
-		this.session = session;
-		this.deviceId = deviceId;
-
-        log.info("[{}]已连接MinitouchSocketServer，sessionid => {}",deviceId,session.getId());
-
-		WebSocketUtil.sendText(session,"minitouch websocket连接成功");
-
-		//检测手机是否连接
-		AndroidDevice androidDevice = AndroidDeviceHolder.get(deviceId);
-		if(androidDevice == null || !androidDevice.isConnected()){
-			log.info("[{}]设备未连接",deviceId);
-			WebSocketUtil.sendText(session,deviceId+"手机未连接");
-			return;
-		}
-		//检测手机是否闲置
-		if(androidDevice.getDevice().getStatus() != Device.IDLE_STATUS){
-			log.info("[{}]设备未处于闲置状态",deviceId);
-			WebSocketUtil.sendText(session,deviceId+"设备未处于闲置状态");
-			return;
-		}
-
-		//如果有其他连接在使用该手机，则不往下处理并提示用户该手机在被占用
-        Session otherSession = minitouchSessionMap.get(deviceId);
-        if(otherSession!=null && otherSession.isOpen()){
-            log.info("[{}]检查到有其他连接正在使用minitouch服务,不做任何处理，当前sessionid => {}，正在使用minitouch的连接sessionid => {}",deviceId,session.getId(),otherSession.getId());
-            WebSocketUtil.sendText(session,deviceId+"手机正在被"+otherSession.getId()+"连接占用,请稍后重试");
+        androidDevice = AndroidDeviceHolder.get(deviceId);
+        if (androidDevice == null || !androidDevice.isConnected()) {
+            basicRemote.sendText(deviceId + "手机未连接");
             return;
         }
 
-		//将设备id和session放入map
-		minitouchSessionMap.put(deviceId,session);
+        if (androidDevice.getDevice().getStatus() != Device.IDLE_STATUS) {
+            basicRemote.sendText(deviceId + "设备未处于闲置状态，" + androidDevice.getDevice().getUsername() + "使用中");
+            return;
+        }
 
-		WebSocketUtil.sendText(session,"开始启动手机minitouch服务");
-		//1.启动手机minitouch服务
-		minitouch = new Minitouch(androidDevice);
-		minitouch.start();
-		WebSocketUtil.sendText(session,"启动手机minitouch服务成功");
+        Session otherSession = sessionPool.get(deviceId);
+        if (otherSession != null && otherSession.isOpen()) {
+            basicRemote.sendText(deviceId + "手机正在被" + otherSession.getId() + "连接占用，请稍后重试");
+            return;
+        }
 
-	}
+        sessionPool.put(deviceId, session);
 
+        basicRemote.sendText("启动minitouch服务...");
+        minitouch = new Minitouch(androidDevice);
+        minitouch.start();
+        basicRemote.sendText("启动minitouch服务完成");
+    }
 
-	/**
-	 * minitouc websocket关闭
-	 *
-	 * 由于用户直接关闭控制手机的网页时，minicap websocket有时关闭很慢，而minitouch每次都很快，所以在minitouch socket断开后，把所有需要回收的资源都放在此处做处理
-	 *
-	 * 设备拔线离线需要回收资源，也是通过关闭minitouch websocket连接来回收stf资源
-	 */
-	@OnClose
-	public void onClose() {
-        log.info("[{}]断开MinitouchSocketServer连接，sessionid => {}",deviceId,session.getId());
-		//由于可能有多个连接访问进来，只允许一个连接使用，意味着只有minitouch被成功初始化的连接才是占用的连接，占用连接断开才做资源回收操作
-        if(minitouch!=null){
-			//断开连接后 移除map里的session
-            minitouchSessionMap.remove(deviceId);
-            //释放minitouch资源
-			minitouch.stop();
-		}
+    @OnClose
+    public void onClose() {
+        log.info("[{}][minitouch][socketserver]onClose", deviceId);
 
-	}
+        if (minitouch != null) {
+            sessionPool.remove(deviceId);
+            minitouch.stop();
+        }
+    }
 
+    @OnError
+    public void onError(Throwable t) {
+        log.error("[{}][minitouch][socketserver]onError", deviceId, t);
+    }
 
-	@OnMessage
-	public void onMessage(String message) {
-		handleMessage(message);
-	}
+    @OnMessage
+    public void onMessage(String message) {
+        handleMessage(message);
+    }
 
-	@OnError
-	public void onError(Throwable t){
-		log.error("[{}]minitouch onError，sessionid => {}",deviceId,session.getId(),t);
-	}
-
-
-	private void handleMessage(String msg){
-		JSONObject jsonObject = JSON.parseObject(msg);
-		String operation = (String) jsonObject.get("operation");
-		//将前端传递过来的尺寸比例乘以minitou输出的屏幕尺寸 得到坐标
-		switch (operation){
-			case "m":
-				minitouch.moveTo(objToFloat(jsonObject.get("pX")), objToFloat(jsonObject.get("pY")));
-				break;
-			case "d":
-				minitouch.touchDown(objToFloat(jsonObject.get("pX")), objToFloat(jsonObject.get("pY")));
-				break;
-			case "u":
-				minitouch.touchUp();
-				break;
-//			case "g":
-//				minitouch.inputKeyevent(3);
-//				break;
-//			case "b":
-//				minitouch.inputKeyevent(4);
-//				break;
-//			case "p":
-//				minitouch.inputKeyevent(26);
-//				break;
-//			case "menu":
-//				minitouch.inputKeyevent(82);
-//				break;
-		}
-	}
-
-	private float objToFloat(Object o){
-		if(o instanceof BigDecimal){
-			return  ((BigDecimal)o).floatValue() ;
-		}else {
-			return  ((Integer)o).floatValue() ;
-		}
-	}
+    /**
+     * 根据前端传来的操作，调用minitouch执行相应操作
+     *
+     * @param msg
+     */
+    private void handleMessage(String msg) {
+        JSONObject jsonObject = JSON.parseObject(msg);
+        String operation = jsonObject.getString("operation");
+        switch (operation) {
+            case "m":
+                minitouch.moveTo(jsonObject.getFloat("pX"), jsonObject.getFloat("pY"));
+                break;
+            case "d":
+                minitouch.touchDown(jsonObject.getFloat("pX"), jsonObject.getFloat("pY"));
+                break;
+            case "u":
+                minitouch.touchUp();
+                break;
+            case "home":
+                try {
+                    AndroidUtils.inputKeyevent(androidDevice.getIDevice(), 3);
+                } catch (Exception e) {
+                    log.error("[{}][minitouch][socketserver]exec home error", deviceId, e);
+                }
+                break;
+            case "back":
+                try {
+                    AndroidUtils.inputKeyevent(androidDevice.getIDevice(), 4);
+                } catch (Exception e) {
+                    log.error("[{}][minitouch][socketserver]exec back error", deviceId, e);
+                }
+                break;
+            case "power":
+                try {
+                    AndroidUtils.inputKeyevent(androidDevice.getIDevice(), 26);
+                } catch (Exception e) {
+                    log.error("[{}][minitouch][socketserver]exec power error", deviceId, e);
+                }
+                break;
+            case "menu":
+                try {
+                    AndroidUtils.inputKeyevent(androidDevice.getIDevice(), 82);
+                } catch (Exception e) {
+                    log.error("[{}][minitouch][socketserver]exec menu error", deviceId, e);
+                }
+                break;
+        }
+    }
 }
