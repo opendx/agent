19c49bed30724469fad34bf3eda5c85c3ac351e1
jiangyitao <451988022@163.com>
2019-05-01 22:04:57 +0800

    update

--- core/src/main/java/com/fgnb/controller/DebugController.java
+++ core/src/main/java/com/fgnb/controller/DebugController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-import static io.restassured.RestAssured.get;
 
 /**
  * Created by jiangyitao.
@@ -17,22 +16,6 @@ import static io.restassured.RestAssured.get;
 @RequestMapping("/debug")
 @Slf4j
 public class DebugController {
-    /**
-     * 检查设备是否可以调试action
-     * @return
-     */
-    @GetMapping("/checkDeviceCanDebugAction")
-    public Response checkDeviceCanDebugAction(Integer uiautomator2Port){
-        try {
-            String pingResp = get("http://127.0.0.1:" + uiautomator2Port + "/wd/hub/session/888/ping").asString();
-            if("pong".equals(pingResp)){
-                return Response.success("当前端口可调试action");
-            }
-        }catch (Exception e){
-            log.error("检查设备是否可以调试action出错",e);
-        }
-        return Response.fail("无法调试action，请先选择一台设备进行远程控制");
-    }
 
     /**
      * 调试action
