809a92bc449a94bb4873c0a7bff0cf09e5a867bc
jiangyitao <451988022@163.com>
2019-05-03 22:42:01 +0800

    update

--- core/src/main/java/com/fgnb/controller/ActionController.java
+++ core/src/main/java/com/fgnb/controller/ActionController.java
@@ -3,7 +3,6 @@ package com.fgnb.controller;
 import com.fgnb.model.Response;
 import com.fgnb.model.request.ActionDebugRequest;
 import com.fgnb.service.ActionService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +13,6 @@ import javax.validation.Valid;
  */
 @RestController
 @RequestMapping("/action")
-@Slf4j
 public class ActionController {
 
     @Autowired
