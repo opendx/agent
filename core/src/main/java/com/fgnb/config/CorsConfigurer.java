52dbb6e56404014dd47c358a1101f4b4cec28947
jiangyitao <451988022@163.com>
2019-05-19 00:47:39 +0800

    update

---
+++ core/src/main/java/com/fgnb/config/CorsConfigurer.java
@@ -0,0 +1,19 @@
+package com.fgnb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by jiangyitao.
+ */
+@Configuration
+public class CorsConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true);
+    }
+}
