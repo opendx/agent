7e15044a89cafd073819dd4baacba028fce492bb
jiangyitao <451988022@163.com>
2019-07-29 18:17:58 +0800

    prepare for ios

--- src/main/java/com/daxiang/core/ios/DefaultIosDeviceChangeListener.java
+++ src/main/java/com/daxiang/core/ios/DefaultIosDeviceChangeListener.java
@@ -1,16 +1,30 @@
 package com.daxiang.core.ios;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+
 /**
  * Created by jiangyitao.
  */
+@Slf4j
+@Component
 public class DefaultIosDeviceChangeListener implements IosDeviceChangeListener {
+
     @Override
     public void onDeviceConnected(String deviceId) {
-
+        new Thread(() -> iosDeviceConnected(deviceId)).start();
     }
 
     @Override
     public void onDeviceDisConnected(String deviceId) {
+        new Thread(() -> iosDeviceDisconnected(deviceId)).start();
+    }
+
+    private void iosDeviceConnected(String deviceId) {
+    }
+
+    private void iosDeviceDisconnected(String deviceId) {
 
     }
 }
