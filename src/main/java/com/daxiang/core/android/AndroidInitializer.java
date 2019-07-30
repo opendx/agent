1dbee196a273aa694db62f1da5d7fb1996d888cd
jiangyitao <451988022@163.com>
2019-07-30 11:16:32 +0800

    prepare for ios

--- src/main/java/com/daxiang/core/android/AndroidInitializer.java
+++ src/main/java/com/daxiang/core/android/AndroidInitializer.java
@@ -1,6 +1,7 @@
 package com.daxiang.core.android;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -14,16 +15,20 @@ import java.io.IOException;
 public class AndroidInitializer implements ApplicationRunner {
 
     @Autowired
-    private AndroidDeviceChangeListener deviceChangeListener;
+    private AndroidDeviceChangeListener androidDeviceChangeListener;
+
+    @Value("${android}")
+    private boolean needAndroid;
 
     @Override
     public void run(ApplicationArguments args) throws IOException, InterruptedException {
-        // agent每次启动都先kill adb server
-        ADB.killServer();
-        Thread.sleep(1000);
-        ADB.startServer();
-        ADB.init();
-        // 添加设备监听器，监听设备连接、断开
-        ADB.addDeviceChangeListener(deviceChangeListener);
+        if (needAndroid) {
+            ADB.killServer();
+            Thread.sleep(1000);
+            ADB.startServer();
+            ADB.init();
+            // 添加设备监听器，监听设备连接、断开
+            ADB.addDeviceChangeListener(androidDeviceChangeListener);
+        }
     }
 }
--- src/main/java/com/daxiang/core/ios/IosInitializer.java
+++ src/main/java/com/daxiang/core/ios/IosInitializer.java
@@ -2,6 +2,7 @@ package com.daxiang.core.ios;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -16,10 +17,15 @@ public class IosInitializer implements ApplicationRunner {
     @Autowired
     private IosDeviceChangeListener iosDeviceChangeListener;
 
+    @Value("${ios}")
+    private boolean needIos;
+
     @Override
     public void run(ApplicationArguments args) {
-        IosDeviceMonitor iosDeviceMonitor = IosDeviceMonitor.getInstance();
-        iosDeviceMonitor.start(iosDeviceChangeListener);
-        log.info("[ios]开始监听设备连接/断开");
+        if (needIos) {
+            IosDeviceMonitor iosDeviceMonitor = IosDeviceMonitor.getInstance();
+            iosDeviceMonitor.start(iosDeviceChangeListener);
+            log.info("[ios]开始监听设备连接/断开");
+        }
     }
 }
