1123d1f2c7bbf9e0be7274a1eadf798a8a05e477
jiangyitao <451988022@163.com>
2019-08-07 10:50:07 +0800

    prepare for ios

--- src/main/java/com/daxiang/core/appium/AppiumServer.java
+++ src/main/java/com/daxiang/core/appium/AppiumServer.java
@@ -1,10 +1,13 @@
 package com.daxiang.core.appium;
 
+import com.daxiang.App;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 
+import java.io.File;
 import java.net.URL;
 
 /**
@@ -18,6 +21,11 @@ public class AppiumServer {
     public void start() {
         AppiumServiceBuilder builder = new AppiumServiceBuilder();
         builder.usingAnyFreePort().withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+        String appiumJs = App.getProperty("appiumJs");
+        if (!StringUtils.isEmpty(appiumJs)) {
+            log.info("[appium-server]appiumJs: {}", appiumJs);
+            builder.withAppiumJS(new File(appiumJs));
+        }
         service = AppiumDriverLocalService.buildService(builder);
 //        service.enableDefaultSlf4jLoggingOfOutputData(); // 输出日志到slf4j
         service.start();
